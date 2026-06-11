8fc113f622a5ecd98796d1c73ab99a1e3fd99e4d
unknown <sonalirath2097@gmail.com>
2026-06-11 18:56:09 +0530

    Fix import in EmployeeServiceImpl

--- backend/src/main/java/com/employee/service/EmployeeServiceImpl.java
+++ backend/src/main/java/com/employee/service/EmployeeServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.employee.entity.Employee;
 import com.employee.repository.EmployeeRepository;
+import com.employee.service.EmployeeService;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
